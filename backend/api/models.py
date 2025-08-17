102de48177ffdebf21ca55c85b9eddf5b760147e
sekibaalamark <markmajje256@gmail.com>
2025-08-17 13:01:50 +0300

    created a PURCHASE model for all purchases

--- backend/api/models.py
+++ backend/api/models.py
@@ -17,4 +17,9 @@ class Sale(models.Model):
     total_price = models.DecimalField(max_digits=10,decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
 
-
+class Purchase(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    total_cost = models.DecimalField(max_digits=10,decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    
