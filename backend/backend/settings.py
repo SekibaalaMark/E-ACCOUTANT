30847f29050544d713bef06cf9b6ac604e02df17
sekibaalamark <markmajje256@gmail.com>
2025-08-17 14:45:21 +0300

    adjusted the expiry of the jwt tokens

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,3 +123,17 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),   # token lasts 120 minutes
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),      # refresh token lasts 7 days
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
