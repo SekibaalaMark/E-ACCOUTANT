7d840a77e53f12b0784a6a9fc63863a251c58193
sekibaalamark <markmajje256@gmail.com>
2025-08-18 15:41:25 +0300

    registered the viewset for product in urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,15 +1,20 @@
 # urls.py
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from .views import *
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
+
+
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path("login/", LoginAPIView.as_view(), name="login"),
-
+    path('', include(router.urls)),
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Login
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh token
