bea1717862dee0b61a95b9bc2cdc5133440f6ba8
sekibaalamark <markmajje256@gmail.com>
2025-08-22 16:48:45 +0300

    added the login functionality with signup button too

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Login from "./Login";
 import Signup from "./Signup";
 import "./App.css";
 
 function App() {
+  const [showSignup, setShowSignup] = useState(false);
+
   return (
     <div>
-      <Signup />
+      {showSignup ? (
+        <Signup />
+      ) : (
+        <Login onSignupClick={() => setShowSignup(true)} />
+      )}
     </div>
   );
 }
